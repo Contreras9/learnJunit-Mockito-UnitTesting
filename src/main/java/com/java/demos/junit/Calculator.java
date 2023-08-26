3544355909d9c75e2c5ec8f1a5dc7522b078cb98
Hamzah Contreras <h.contreras9@outlook.com>
2023-08-26 17:06:59 -0500

    Creating a Java project using Junit

---
+++ src/main/java/com/java/demos/junit/Calculator.java
@@ -0,0 +1,8 @@
+package com.java.demos.junit;
+
+public class Calculator {
+
+    public static int add(int num1, int num2) {
+        return num1 + num2;
+    }
+}
---
+++ src/test/java/com/java/demos/junit/CalculatorTest.java
@@ -0,0 +1,4 @@
+package com.java.demos.junit;
+
+public class CalculatorTest {
+}
